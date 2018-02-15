920e18eed87fc77fb59c0b662ae6b664902ada58
Michael Gacek <gacekmike22@gmail.com>
2018-02-15 14:58:56 -0600

    Add project files.

---
+++ TwoMinuteMovies/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TwoMinuteMovies.Models;
+
+namespace TwoMinuteMovies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        // GET: Movies
+        public ActionResult Index(string searchString)
+        {
+            var movies = from m in db.Movies
+                         select m;
+
+            // Search by Title or Genre
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString) || s.Genre.Equals(searchString)).OrderBy(s => s.Genre);
+            }
+
+            return View(movies);
+        }
+
+    }
+}
---
+++ TwoMinuteMovies/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TwoMinuteMovies.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string YoutubeLink { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
